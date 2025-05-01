4107275fd090ba28b5a7ee6eef8446cf96c549df
peousedutta <duttapeouse@gmail.com>
2025-05-02 01:31:17 +0530

    requirements updated again os sys added

--- utilities/ReadConfigurations.py
+++ utilities/ReadConfigurations.py
@@ -8,6 +8,7 @@ class ReadConfig:
     @staticmethod
     def getApplicationUrl():
         try:
+            print("[DEBUG] -- URL --", config.get('info','baseurl'))
             return config.get('info','baseurl')
         except Exception as err:
             raise Exception(f"[ERROR] Exception occured while fetch base url got --> {err}")
